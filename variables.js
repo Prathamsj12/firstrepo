27402b3ae7aaa6cbdf1a593cd299bfe9be6808aa
Prathamsj12 <prathmesh.jangam80@gmail.com>
2023-11-27 16:30:27 +0530

    files added sucessful

--- variables.js
+++ variables.js
@@ -1,6 +1,32 @@
-let v='prathmesh';
-console.log(v);
-v=4555;
-console.log(v);
-let r=885.50;
-console.log(typeof(r))
+let name='prathmesh';
+console.log(name);
+let amuont=885.50;
+console.log(typeof(amount))
+//String Data Type
+let city="Mumbai"
+console.log(typeof(city));
+//Boolean data type
+let Istrue=false
+console.log(typeof(Istrue));
+//Undefined
+let a;
+console.log(a)
+let Value=null; //data type of the null value is null
+console.log(Value)
+let bigint=BigInt("99999999999999") //@ it the numbers are more than 15 digit then it will show 0 if not define bigint
+console.log(bigint);
+//Array is the collection of the values stored inside the square brackets
+let array=["Prathmesh","Santosh","Jangam"]
+console.log(array[0])
+//Object Object is the collection of the key value pairs in the other programming language object is take as the dictory
+let object=[{name:"Prathmesh",Age:25}]
+console.log(typeof(object.name));
+console.log(object.Age);
+console.log(array.length)
+//Array of object in array of obeject obejcts are present inside the array
+let array_object=[{name:"prathmesh",Age:25},
+{name:"prathmesh",Age:25},
+{name:"prathmesh",Age:25}
+]
+console.log(array_object[0].name)
+console.log(array_object.length)
